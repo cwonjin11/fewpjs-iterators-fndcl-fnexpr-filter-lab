582552d8c8cd55a73836b59a591ada3d41559a15
Wonjin Cho <cwonjin11@gmail.com>
2021-06-07 12:17:45 -0400

    commit tree 6bddf6cf32c56018c05a962cf150d84be9c9f5ed

--- index.js
+++ index.js
@@ -1,1 +1,21 @@
 // Code your solution here
+// const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby']
+
+
+function findMatching(drivers, name) {
+    return drivers.filter( passedInName =>
+        passedInName.toLowerCase() === name.toLowerCase()
+      )
+
+
+}
+
+function fuzzyMatch(source, testString) {
+    return source.filter( possibleMatch =>
+      possibleMatch.toLowerCase().indexOf(testString.toLowerCase()) === 0
+    )
+  }
+  
+  function matchName(source, soughtName) {
+    return source.filter( record => record.name === soughtName)
+  }
